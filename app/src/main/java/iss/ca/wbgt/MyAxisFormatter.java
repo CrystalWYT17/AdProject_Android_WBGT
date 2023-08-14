c218cfcc75a2b1ca55de45e639b5bca85fe8dbfe
Myat Thiha Naing <myatthihanaing.mthn@gmail.com>
2023-08-14 17:33:19 +0800

    fixed linechart bugs

---
+++ app/src/main/java/iss/ca/wbgt/MyAxisFormatter.java
@@ -0,0 +1,17 @@
+package iss.ca.wbgt;
+
+import com.github.mikephil.charting.formatter.ValueFormatter;
+
+public class MyAxisFormatter extends ValueFormatter {
+    @Override
+    public String getFormattedValue(float value) {
+        String res;
+        if (value <= 23){
+            res = String.valueOf(value)+":00";
+        }else {
+            value = value -24;
+            res = String.valueOf(value)+":00";
+        }
+        return res;
+    }
+}
